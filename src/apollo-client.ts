ddd3e318327d42dfed1fd7b2def8dd7c49365a81
Enrique Casanova <enrique.casanova@projekt202.com>
2023-07-09 19:53:16 -0500

    Hasura server side client

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,7 +1,10 @@
 import {ApolloClient, InMemoryCache} from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_HASURA_PROJECT_ENDPOINT,
+  uri: process.env.HASURA_PROJECT_ENDPOINT,
+  headers: {
+    "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET as string,
+  },
   cache: new InMemoryCache(),
 });
 
